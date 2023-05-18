57a380af3e601581e4756397bf8a57e15f87c226
Philip Limbu <philip.limbu@bjss.com>
2023-05-18 13:14:19 +0100

    js addition navbar

---
+++ scripts/include.js
@@ -0,0 +1,35 @@
+async function fetchData(file) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) {
+        throw new Error('Network response was not OK');
+      }
+      const html = await response.text();
+  
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+  
+      return doc;
+    } catch (error) {
+      console.warn('Something went wrong.', error);
+      throw error;
+    }
+  }
+  
+async function insertHtmlIntoDiv(file, divId, activeid) {
+    try {
+      const doc = await fetchData(file);
+  
+      const div = document.getElementById(divId);
+      div.innerHTML = '';
+        div.appendChild(doc.documentElement);
+
+        var els = document.getElementById(activeid)
+        els.classList.add("active")
+
+    } catch (error) {
+      console.warn('Error inserting HTML into div:', error);
+    }
+  }
+  
+ 
